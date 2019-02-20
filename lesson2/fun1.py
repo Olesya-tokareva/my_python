8026fd250329c7a62f006f03f7be3d2b09f2a340
Olesya-tokareva <47825459+Olesya-tokareva@users.noreply.github.com>
2019-02-20 23:25:05 +0300

    Add files via upload

---
+++ lesson2/fun1.py
@@ -0,0 +1,6 @@
+v=float(input('Number: '))
+if -2.4<=v<=5.7:
+    print(v**2)
+else:
+    print(4)
+    
---
+++ lesson2/my_max.py
@@ -0,0 +1,9 @@
+x=float(input('Введите первое число: '))
+y=float(input('Введите второе число: '))
+if x-y>0:
+    print(x)
+elif x-y<0:
+    print(y)
+
+
+
